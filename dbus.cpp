f0eabd7b8633153bbe661af46b88f4723db0eb56
Enrico Zini <enrico@enricozini.org>
2014-10-17 15:45:10 +0200

    Check that the folder actually exists

--- dbus.cpp
+++ dbus.cpp
@@ -9,6 +9,7 @@ BuffyServer::BuffyServer(QApplication& app, Buffy& buffy) :
 void BuffyServer::set_active_inbox(QString folder_name, bool value)
 {
     Folder* f = buffy.folders.by_name(folder_name.toStdString());
+    if (!f) return;
     f->set_active_inbox(value);
 }
 
